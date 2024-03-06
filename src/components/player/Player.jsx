d5a1ab6001a232e4402d3ee5526138e36f58c923
Nizvan MR <njr01397@gmail.com>
2024-03-05 19:02:32 -0600

    Feat: Playlist part 1

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -5,15 +5,15 @@ import CurrentSong from "./CurrentSong";
 import VolumeControl from "./VolumeControl";
 import SongControl from "./SongControl";
 
-export const Play = () => {
+export const Play = ({ size = 'small' }) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" className="text-black icon icon-tabler icon-tabler-player-play-filled" width="18" height="18" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" stroke-width="0" fill="currentColor" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="text-black icon icon-tabler icon-tabler-player-play-filled" width={size == "large" ? 24 : 18} height={size == "large" ? 24 : 18} viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" stroke-width="0" fill="currentColor" /></svg>
     )
 }
 
-export const Pause = () => {
+export const Pause = ({ size = 'small' }) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" className="text-black icon icon-tabler icon-tabler-player-pause-filled" width="18" height="18" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" /><path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="text-black icon icon-tabler icon-tabler-player-pause-filled" width={size == "large" ? 24 : 18} height={size == "large" ? 24 : 18} viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" /><path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" /></svg>
     )
 }
 
